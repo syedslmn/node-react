4fa0bbd9c48af9ff60fa951e3dc4d7bbfd556e65
Syed Sulaiman M <syedsulaiman.m@ideas2it.com>
2016-12-06 12:58:37 +0530

    Port Updated

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var path = require("path");
 
 var app = express();
 app.use(express.static(path.join(__dirname,"../app/dist")));
-app.listen(7777,function(){
-    console.log("Started listening on port", 7777);
+var port = process.env.PORT || 5000;
+app.listen(port,function(){
+    console.log("Started listening on port", port);
 })
